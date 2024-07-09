94a77120a399dc5e37b77b688c6c3d84b1019ce0
mirajhad <mirajhadish48@gmail.com>
2024-07-09 16:30:54 +0530

    navigation

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request,model';
 import { CategoryService } from '../services/category.service';
 import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -15,14 +16,14 @@ export class AddCategoryComponent implements OnDestroy {
   private addCategorySubscribtion?: Subscription;
 
 
-  constructor(private categoryService: CategoryService) {
+  constructor(private categoryService: CategoryService, private router: Router) {
     this.model = {
       name:'',
       urlHandle:''
     };
   }
   ngOnDestroy(): void {
-    this.addCategorySubscribtion?.unsubscribe();  
+    this.addCategorySubscribtion?.unsubscribe();
   }
 
   onFormSubmit(){
@@ -30,7 +31,7 @@ export class AddCategoryComponent implements OnDestroy {
    this.addCategorySubscribtion= this.categoryService.addCategory(this.model)
     .subscribe({
       next: (data) => {
-        console.log(data);
+        this.router.navigateByUrl('/admin/categories')
       }
     })
   }
