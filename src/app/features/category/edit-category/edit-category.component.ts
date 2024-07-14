fce093948b6d22ebc9194fa2dee4095771cb81b9
mirajhad <mirajhadish48@gmail.com>
2024-07-14 21:54:42 +0530

    edit component

---
+++ src/app/features/category/edit-category/edit-category.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {  Subscription } from 'rxjs';
+import { CategoryService } from '../services/category.service';
+import { Category } from '../models/category.model';
+
+@Component({
+  selector: 'app-edit-category',
+  templateUrl: './edit-category.component.html',
+  styleUrls: ['./edit-category.component.css']
+})
+export class EditCategoryComponent implements OnInit, OnDestroy {
+
+  id:string | null = null;
+  paramsSubcsription?: Subscription;
+  category?: Category;
+
+
+  constructor(private route: ActivatedRoute, private categoryService: CategoryService) { }
+  ngOnDestroy(): void {
+    this.paramsSubcsription?.unsubscribe();
+  }
+
+  ngOnInit(): void {
+   this.paramsSubcsription= this.route.paramMap.subscribe({
+      next: (params) => {
+      this.id =  params.get('id')
+
+      if(this.id){
+        this.categoryService.getCategoryById(this.id).subscribe({
+          next: (category) => {
+            this.category = category;
+          }
+        })
+      }
+
+      }
+    })
+  }
+
+  onFormSubmit():void{
+    console.log(this.category)
+  }
+}
