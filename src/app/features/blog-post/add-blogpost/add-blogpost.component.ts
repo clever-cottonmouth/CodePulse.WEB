3d9e1d6ca3c64873949918865685279f874af7d4
mirajhad <mirajhadish48@gmail.com>
2024-07-17 02:04:28 +0530

    blogpost added

---
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+
+@Component({
+  selector: 'app-add-blogpost',
+  templateUrl: './add-blogpost.component.html',
+  styleUrls: ['./add-blogpost.component.css']
+})
+export class AddBlogpostComponent {
+  model: AddBlogPost;
+
+  constructor() {
+    this.model = {
+      title: '',
+      shortDescription: '',
+      content: '',
+      featuredImageUrl: '',
+      urlHandle: '',
+      author: '',
+      isVisible: true,
+      publishedDate: this.formatDate(new Date())
+    };
+  }
+  formatDate(date: Date): Date {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return new Date(`${year}-${month}-${day}`);
+  }
+
+  onFormSubmit():void{
+    console.log(this.model)
+  }
+}
